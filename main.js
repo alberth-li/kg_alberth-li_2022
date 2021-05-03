f73b1026073f43a19f60fc01668fac1c8641ceb0
ahpli <ahpli@umich.edu>
2021-05-03 17:35:39 -0400

    Improve formats
    Add file header. Improve cod formats.

--- main.js
+++ main.js
@@ -1,32 +1,44 @@
+/**
+ * Convert an array of integers into an array of strings representing the phonetic equivalent of the integer.
+ *
+ * @file This files defines a program that convert array of integers to phonetic alphabet.
+ *
+ * @author Albert Li.
+ *
+ * @since 05.03.2021
+ *
+ * @version 1.0(FINAL)
+ */
+
 //get input
 let input = process.argv.slice(2);
 
 //setup phonetic dic
-const PHONETIC = ['Zero','One','Two','Three','Four','Five','Six','Seven','Eight','Nine'];
+const PHONETIC = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
 
 /**
  * Single digit to alphabet converter.
- * 
+ *
  * Convert input single digit to corresponding alphabet as output
- * 
+ *
  * @param {Number} digit single digit to be converted to alphabet.
- * 
+ *
  * @returns {String} converted alphabet
  */
-let numberToAlpha = function(digit){
+let numberToAlpha = function (digit) {
     return PHONETIC[digit];
 }
 
 /**
  * Whole number to phonetic equivalent converter.
- * 
+ *
  * Convert input whole number to their phonetic equivalent.
- * 
+ *
  * @param {String} number string representation of a whole number to be converted.
- * 
- * @returns {String} single string of concatenated convertion result
+ *
+ * @returns {String} single string of concatenated conversion result
  */
-let convertFunction = function(number){
+let convertFunction = function (number) {
     //split the number to array of single digits
     //map numberToAlpha function and name as convertedNumber
     const convertedNumber = number.split('').map(numberToAlpha);
